8f51e4f41254280f9b215abdbb51a741ebc87a90
Dattathreya <sonnathidattathreya@gmail.com>
2018-03-14 01:32:58 +0530

    commit undo

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var path = require('path');
 
 var ejs = require('ejs');
 
-var jade = require('jade');
+//var jade = require('jade');
 
 var app = express();
 
@@ -34,10 +34,57 @@ server.listen(port);
 
 app.set('view engine','ejs');
 
-app.set('view engine' ,'jade');
+//app.set('view engine' ,'jade');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 var router = express.Router();
 
 app.use(router);
+
+var appleSchema = new mongoose.Schema({
+
+    fullname:String,
+    fname:String,
+    lname:String,
+    email:String,
+    password:String,
+    rpassword:String,
+    gender:String,
+    age:String,
+    address:String,
+    message:String
+
+
+
+    });
+
+var apple = mongoose.model("apple",appleSchema);
+
+    router.post('/application', function(req, res, next) {
+
+        console.log(req.body);
+
+        var graps = new apple({
+
+        });
+    });
+
+    graps.save(function (err, graps){
+
+            if(err){
+                console.log(err);
+                res.json({"status":err})}
+
+            else{
+                console.log(graps);
+                res.json({"status":"success"})
+
+            }
+    });
+
+
+    router.post('/application', function(req,res,next){
+
+
+    });
